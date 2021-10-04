404ab8a4fd50dfd0c36eb7faf6ee18af4fc817fa
VladimirStorm777 <firstbens@gmail.com>
2021-10-04 07:07:38 +0500

    anon-func-commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
-
 void main() => runApp(MyAppVova());
 
 class MyAppVova extends StatelessWidget {
   const MyAppVova({Key? key}) : super(key: key);
-
+  void answerQuestion() {
+    print('Answer chosen');
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My First Appbar'),       ),
-        body: Column(
-          // ignore: prefer_const_literals_to_create_immutables
-          children: [
-            const Text('My Questions'),
-            // ignore: deprecated_member_use
-            const RaisedButton(child: Text('answer1'), onPressed: null),
-            const RaisedButton(child: Text('answer2'), onPressed: null),
-            const RaisedButton(child: Text('answer3'), onPressed: null),
-          ],    ),   ),  );  }}
+        home: Scaffold(
+            appBar: AppBar(title: const Text('my first app')),
+            body: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text('list of buttons'),
+                  RaisedButton(
+                      child: const Text('btn1'), onPressed: answerQuestion),
+                  RaisedButton(
+                      child: const Text('btn2'),
+                      onPressed: () => print('btn2 is pressed')), //аноним. ф-ия
+                  RaisedButton(
+                      child: const Text('btn3'),
+                      onPressed: () {          //аноним. ф-ия
+                        print('btn3 is pressed!!!');
+                      }),
+                  RaisedButton(
+                      child: const Text('btn4'), onPressed: answerQuestion)
+                ])));  }}
